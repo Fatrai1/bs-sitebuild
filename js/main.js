06fe422215443cd28786c5936151364ff5235ce5
Fatrai2 <midgard@citromail.hu>
2021-01-11 09:02:10 +0100

    nav

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+const navbar = document.querySelector('.nav');
+const sticky = navbar.offsetTop;
+
+function stickyNavBarFunction() {
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky");
+        navbar.classList.add("bg-light");
+        navbar.classList.add("text-blue");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+window.addEventListener('scroll', stickyNavBarFunction);
